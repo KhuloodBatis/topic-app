b038a76db7b02147698089fffbb10ebedc2d0c72
khulood batis <khuloodbatis@gmail.com>
2022-03-26 12:08:18 +0300

    kool

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use App\Models\Topic;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory;
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function topic(): BelongsTo
+    {
+        return $this->belongsTo(Topic::class);
+    }
+}
---
+++ app/Models/Topic.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Topic extends Model
+{
+    use HasFactory;
+
+
+    /**
+     * Get the user that owns the Topic
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class,);
+    }
+
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+}
